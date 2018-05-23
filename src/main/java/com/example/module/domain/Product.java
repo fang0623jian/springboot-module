c39ba7ffefc836e94f2086dcc0f1f104d39c2cf8
fangjian <fang_jian@gowild.cn>
2018-05-23 18:52:56 +0800

    修改genetator代码生成工具相关配置并测试

---
+++ src/main/java/com/example/module/domain/Product.java
@@ -0,0 +1,80 @@
+package com.example.module.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer amount;
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return amount
+     */
+    public Integer getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount
+     */
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", amount=").append(amount);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Product other = (Product) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getAmount() == null ? other.getAmount() == null : this.getAmount().equals(other.getAmount()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getAmount() == null) ? 0 : getAmount().hashCode());
+        return result;
+    }
+}
---
+++ src/main/java/com/example/module/mapper/ProductMapper.java
@@ -0,0 +1,7 @@
+package com.example.module.mapper;
+
+import com.example.module.domain.Product;
+import tk.mybatis.mapper.common.BaseMapper;
+
+public interface ProductMapper extends BaseMapper<Product> {
+}
